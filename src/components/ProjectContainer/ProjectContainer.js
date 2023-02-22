a8dd65d0c99c76c5f477a67ac9075d17fe267215
JIN HO LEE <jinholee@wirelessprv-10-194-0-178.near.illinois.edu>
2023-02-21 21:15:31 -0600

    update previews

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -6,7 +6,6 @@ import './ProjectContainer.css'
 const ProjectContainer = ({ project }) => (
   <div className='project'>
     <h3>{project.name}</h3>
-
     <p className='project__description'>{project.description}</p>
     {project.stack && (
       <ul className='project__stack'>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://jinhohl2.github.io/Portfolio/',
+  title: 'Portfolio',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Jinho Lee',
+  role: 'Master Student in Computer Science',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am a .',
+  resume: 'https://drive.google.com/file/d/18dcolKelO_jX1IGeYejMhadFAGrhF97r/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/jin-ho-lee-a66191153/',
+    github: 'https://github.com/jinhohl2',
   },
 }
 
@@ -21,27 +21,44 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Novus-J-Mate Restaurant App',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+    stack: ['TypeScript', 'React', 'MongoDB', 'Node','Express'],
+    sourceCode: 'https://github.com/jinhohl2/Novus-J-Mate-Restaurant',
+    livePreview: 'https://27guptamohit.gitlab.io/novus-j-mate-restaurant/',
+  },
+  {
+    name: '3D Reconstruction with Radon Transformation',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['Python', 'Computer Vision'],
+    sourceCode: 'https://github.com/jinhohl2/3D-Reconstruction-of-Medical-Images-with-Radom-Transformation',
+    livePreview: 'https://drive.google.com/file/d/1TptasmPkFpJjEO-C_m44Z4Z2pdB2-wix/view?usp=sharing',
+  },
+  {
+    name: 'Field Efficiency Visualization',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['Javascript', 'Deck.gl', 'amChart'],
+    sourceCode: 'https://github.com/jinhohl2/fieldefficiency',
+    livePreview: 'https://drive.google.com/file/d/1pV2zYLhXFCNM-DUAaMsEksPTXeoBHSxV/view?usp=sharing',
+  },
+  {
+    name: 'Face Recognition Model with Graph Structure',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['Deep Learning/AI', 'CNN', 'GNN'],
+    sourceCode: 'https://github.com/jinhohl2/Face_Recognition_with_Graph',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Optimal Item Resource Allocation',
+    visual: "",
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+    stack: ['Python', 'Convex Optimization'],
+    sourceCode: 'https://github.com/jinhohl2/Multiple_Item_Allocation',
     livePreview: 'https://github.com',
   },
 ]
@@ -49,23 +66,22 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
+  'Python',
+  'C++',
   'JavaScript',
   'TypeScript',
+  'HTML',
+  'CSS',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Node',
+  'ML/AI',
+  'Computer Vision',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'jinhohl2@illinois.edu',
 }
 
 export { header, about, projects, skills, contact }
