b5bf13248506efe6da93b6e512f0432132ac924b
JIN HO LEE <0416roylee@gmail.com>
2023-10-31 22:34:27 -0500

    changed url

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://jinhohl2.github.io/Portfolio/',
+  homepage: 'https://jinhoroy.github.io/Portfolio/',
   title: 'Portfolio',
 }
 
